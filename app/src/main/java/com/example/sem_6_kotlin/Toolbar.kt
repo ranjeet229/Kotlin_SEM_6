a8ed1a9c936ac753b5648f1dd2816d92bb957a06
Ranjeet <kiranjeetkr80@gmail.com>
2025-03-06 01:45:19 +0530

    toolbar

--- app/src/main/java/com/example/sem_6_kotlin/Toolbar.kt
+++ app/src/main/java/com/example/sem_6_kotlin/Toolbar.kt
@@ -1,56 +1,36 @@
 package com.example.sem_6_kotlin
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class Toolbar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_toolbar)
 
-        val layout = LinearLayout(this).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            orientation = LinearLayout.VERTICAL
-        }
-
-        val toolbar = Toolbar(this).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-            setBackgroundColor(Color.BLUE)
-            setTitleTextColor(Color.WHITE)
-            title = "Toolbar"
-        }
-
-        // Set the toolbar as the action bar
-        layout.addView(toolbar)
-        setContentView(layout)
-
-        setSupportActionBar(toolbar) // This must be called after setContentView
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.custom_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> {
-                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.group -> Toast.makeText(this, "Create a new group", Toast.LENGTH_SHORT).show()
+            R.id.broadcast -> Toast.makeText(this, "Create a new broadcast", Toast.LENGTH_SHORT).show()
+            R.id.Linked -> Toast.makeText(this, "Check linked devices", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "Go to settings", Toast.LENGTH_SHORT).show()
+            else -> return super.onOptionsItemSelected(item)
         }
+        return true
     }
 }
